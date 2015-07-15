bb0414eb0126b7b73302c774ce43e3444f13c415
David Bimmler <bimmlerd@student.ethz.ch>
2015-07-15 14:49:30 +0200

    steps

--- MuscleBot.py
+++ MuscleBot.py
@@ -8,7 +8,10 @@ __author__ = 'bimmlerd@student.ethz.ch'
 
 import telegram
 import requests
+import json
 
+#BASE_URL = "http://muscle.bimmler.ch/api/"
+BASE_URL = "http://localhost:3000/api/"
 
 def main():
     f = open("AuthToken")
@@ -27,9 +30,24 @@ def main():
 
             if LAST_UPDATE_ID < update_id:  # If newer than latest update
                 if text:
-                    response = "You said \"%s\"" % text 
+                    if text == "/list":
+                        response = "You wanted the latest events:"
+                    elif text == "/join":
+                        response = "These events are available:\n"
+                        events = getEvents()
+                        for i, event in enumerate(events):
+                            response += "%d.\t%s\n" % (i, event['name'])
+
+                    else:
+                        response = "That's not something I understand" + telegram.Emoji.PILE_OF_POO
                     bot.sendMessage(chat_id=chat_id, text=response)
                     LAST_UPDATE_ID = update_id
+
+def getEvents():
+    url = BASE_URL + "events"
+    r = requests.get(url)
+    data = json.loads(r.text)['data']
+    return data
 
 if __name__ == '__main__':
     main()
